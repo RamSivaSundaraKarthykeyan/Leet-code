1a483b65fbd5f44c5c595e815e7677043da382db
Ram Siva Sundara Karthykeyan <140699334+RamSivaSundaraKarthykeyan@users.noreply.github.com>
2025-10-22 06:22:02 +0530

    leet problem on 22/10
    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
    
    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
    
     
    
    Example 1:
    
    
    Input: l1 = [2,4,3], l2 = [5,6,4]
    Output: [7,0,8]
    Explanation: 342 + 465 = 807.
    Example 2:
    
    Input: l1 = [0], l2 = [0]
    Output: [0]
    Example 3:
    
    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
    Output: [8,9,9,9,0,0,0,1]
     
    
    Constraints:
    
    The number of nodes in each linked list is in the range [1, 100].
    0 <= Node.val <= 9
    It is guaranteed that the list represents a number that does not have leading zeros.

---
+++ linkedListAddition.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+ function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode();
+    let res = dummy;
+    let total = 0, carry = 0;
+
+    while (l1 || l2 || carry) {
+        total = carry;
+
+        if (l1) {
+            total += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            total += l2.val;
+            l2 = l2.next;
+        }
+
+        let num = total % 10;
+        carry = Math.floor(total / 10);
+        dummy.next = new ListNode(num);
+        dummy = dummy.next;
+    }
+
+    return res.next;
+};
+
+let l1 = [2,4,3];
+let l2 = [5,6,4];
+console.log(addTwoNumbers(l1, l2));
