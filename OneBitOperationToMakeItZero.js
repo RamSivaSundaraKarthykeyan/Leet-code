64ab8f0988e602f70277ad1ce035d7fbf9610f24
Ram Siva Sundara Karthykeyan <140699334+RamSivaSundaraKarthykeyan@users.noreply.github.com>
2025-11-08 23:10:25 +0530

    leet daily code on 08/11/25
    Given an integer n, you must transform it into 0 using the following operations any number of times:
    
    Change the rightmost (0th) bit in the binary representation of n.
    Change the ith bit in the binary representation of n if the (i-1)th bit is set to 1 and the (i-2)th through 0th bits are set to 0.
    Return the minimum number of operations to transform n into 0.
    
     
    
    Example 1:
    
    Input: n = 3
    Output: 2
    Explanation: The binary representation of 3 is "11".
    "11" -> "01" with the 2nd operation since the 0th bit is 1.
    "01" -> "00" with the 1st operation.
    Example 2:
    
    Input: n = 6
    Output: 4
    Explanation: The binary representation of 6 is "110".
    "110" -> "010" with the 2nd operation since the 1st bit is 1 and 0th through 0th bits are 0.
    "010" -> "011" with the 1st operation.
    "011" -> "001" with the 2nd operation since the 0th bit is 1.
    "001" -> "000" with the 1st operation.
     
    
    Constraints:
    
    0 <= n <= 109

---
+++ OneBitOperationToMakeItZero.js
@@ -0,0 +1,13 @@
+const minimumOneBitOperations = n => {
+    const s = n.toString(2);
+
+    let b =  s[0];
+
+    for (let i = 1; i < s.length; i++)
+        if (b[i - 1] === s[i])
+            b += '0';
+        else
+            b += '1';
+
+    return parseInt(b, 2);
+};
