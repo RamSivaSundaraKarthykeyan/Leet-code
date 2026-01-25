4c3c25dc0bcdc559c39cedc5217107d27f8e6d0e
Ram Siva Sundara Karthykeyan <140699334+RamSivaSundaraKarthykeyan@users.noreply.github.com>
2026-01-26 00:00:29 +0530

    Leet weekly contest on 25/1/26
    You are given an integer array nums.
    
    You need to remove exactly one prefix (possibly empty) from nums.
    
    Return an integer denoting the minimum length of the removed such that the remaining array is .
    
     
    
    Example 1:
    
    Input: nums = [1,-1,2,3,3,4,5]
    
    Output: 4
    
    Explanation:
    
    Removing the prefix = [1, -1, 2, 3] leaves the remaining array [3, 4, 5] which is strictly increasing.
    
    Example 2:
    
    Input: nums = [4,3,-2,-5]
    
    Output: 3
    
    Explanation:
    
    Removing the prefix = [4, 3, -2] leaves the remaining array [-5] which is strictly increasing.
    
    Example 3:
    
    Input: nums = [1,2,3,4]
    
    Output: 0
    
    Explanation:
    
    The array nums = [1, 2, 3, 4] is already strictly increasing so removing an empty prefix is sufficient.

---
+++ Minimum Prefix Removal to Make Array Strictly Increasing.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumPrefixLength = function(nums) {
+    let res = 0;
+    for(let i=1; i<nums.length; i++){
+        if(nums[i]<=nums[i-1]){
+            res=i;
+        }
+    }
+    return res;
+};
